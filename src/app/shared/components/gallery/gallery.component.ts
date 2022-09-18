cf884d22f968318a2328cc0a9f8f3e8aa2564624
kelly.lopes <kelly.lopes@maida.health>
2022-09-18 19:00:49 -0300

    adicionado paginação

--- src/app/shared/components/gallery/gallery.component.ts
+++ src/app/shared/components/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ViewChildren, ElementRef, QueryList, AfterViewInit } from '@angular/core';
 import { Image } from '../../models/image';
 import { ImageService } from '../../services/image.service';
 
@@ -10,28 +10,54 @@ import { Subscription, Observable } from 'rxjs';
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
-export class GalleryComponent implements OnInit, OnDestroy   {
+export class GalleryComponent implements OnInit, AfterViewInit, OnDestroy   {
+
+  @ViewChild('scroll', {static: false}) scroll!: ElementRef;
+  private observer!: IntersectionObserver;
 
   public imageList: Image[] = []
   public listSize: number = 50;
+  public fullHDWidth: number = 1080;
+  public fullHDHeight: number = 720;
+  public mqAlias: string = ''
 
-  watcher!: Subscription;
-  media$!: Observable<MediaChange[]>;
-  activeMediaQuery!: MediaChange;
-  mqAlias: string = ''
+  private watcher!: Subscription;
+  private media$!: Observable<MediaChange[]>;
+  private activeMediaQuery!: MediaChange;
 
   constructor(private ImageService: ImageService, private media: MediaObserver) {
     this.media$ = media.asObservable();
     this.watcher = this.media$.subscribe(change => {
       this.activeMediaQuery = change[0];
       this.mqAlias = this.activeMediaQuery.mqAlias;
-      console.log(change[0].mqAlias)
     });
   }
 
   ngOnInit(): void {
-    
     this.imageList = this.ImageService.getImagemList(this.listSize);
+  }
+
+  ngAfterViewInit(): void {
+    this.generateObserver();
+  }
+
+  generateObserver() {
+    let options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5,
+    }
+    this.observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        console.log('scroll');
+        this.ImageService.getImagemList(5).forEach(image => {
+          this.imageList.push(image);
+        })
+        console.log(this.imageList);
+      }
+    }, options);
+
+    this.observer.observe(this.scroll.nativeElement);
   }
 
   ngOnDestroy(): void {
