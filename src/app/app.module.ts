4c2f3a0904204621db74823c661b8ff052b60f91
kelly.lopes <kelly.lopes@maida.health>
2022-09-18 19:59:20 -0300

    adicionado componente fade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './shared/components/components.module';
@@ -12,7 +13,8 @@ import { ComponentsModule } from './shared/components/components.module';
   imports: [
     BrowserModule,
     FlexLayoutModule,
-    ComponentsModule
+    ComponentsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/components/fade/fade.component.ts
+++ src/app/shared/components/fade/fade.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  animate,
+  AnimationEvent,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+
+export type FadeState = 'visible' | 'hidden';
 
 @Component({
   selector: 'app-fade',
   templateUrl: './fade.component.html',
-  styleUrls: ['./fade.component.css']
+  styleUrls: ['./fade.component.css'],
+  animations: [
+    trigger('state', [
+      state(
+        'visible',
+        style({
+          opacity: '1'
+        })
+      ),
+      state(
+        'hidden',
+        style({
+          opacity: '0'
+        })
+      ),
+      transition('* => visible', [animate('500ms ease-out')]),
+      transition('visible => hidden', [animate('500ms ease-out')])
+    ])
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FadeComponent implements OnInit {
 
+  @Input() 
+  set show(value: boolean) {
+    if (value) {
+      this._show = value;
+      this.state = 'visible';
+    } else {
+      this.state = 'hidden';
+    }
+  }
+
+  get show() {
+    return this._show;
+  }
+
+  private _show!: boolean;
+  state!: FadeState;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  animationDone(event: AnimationEvent | any) {
+    if (event.fromState === 'visible' && event.toState === 'hidden') {
+      this._show = false;
+    }
   }
 
 }
