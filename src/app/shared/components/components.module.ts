4411a55db80dc86307801e05b5bb000162282a53
kelly.lopes <kelly.lopes@maida.health>
2022-09-20 22:06:50 -0300

    adicionado componente de imagem

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -6,9 +6,10 @@ import { GradientComponent } from './gradient/gradient.component';
 import { FadeComponent } from './fade/fade.component';
 import { TopnavComponent } from './topnav/topnav.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ImageComponent } from './image/image.component';
 
 @NgModule({
-  declarations: [GalleryComponent, GradientComponent, FadeComponent,TopnavComponent],
+  declarations: [GalleryComponent, GradientComponent, FadeComponent,TopnavComponent, ImageComponent],
   imports: [
     CommonModule,
     FontAwesomeModule,
